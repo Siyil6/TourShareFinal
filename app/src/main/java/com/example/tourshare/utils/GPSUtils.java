fcf01633de8b52fccd83fa32013f1889a87235bb
Benchen997 <89903375+Benchen997@users.noreply.github.com>
2022-11-02 13:29:19 +1100

    project info

--- app/src/main/java/com/example/tourshare/utils/GPSUtils.java
+++ app/src/main/java/com/example/tourshare/utils/GPSUtils.java
@@ -45,7 +45,7 @@ public class GPSUtils {
 
         // Determine whether the GPS is activated normally
         if (!mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            Toast.makeText(context, "请开启GPS导航...", Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, "Please Open GPS...", Toast.LENGTH_SHORT).show();
             // Return to the GPS navigation setting interface
             Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
             context.startActivityForResult(intent, 0);
@@ -66,9 +66,12 @@ public class GPSUtils {
         // Binding listener, there are 4 parameters
         // Parameter 1, device: there are two kinds of GPS_PROVIDER and NETWORK_PROVIDER
         // Parameter 2, location information update cycle, in milliseconds
-        // Parameter 3, the minimum distance of position change: when the change of position distance exceeds this value, the position information will be updated
+        // Parameter 3, the minimum distance of position change: when the change of position distance
+        // exceeds this value, the position information will be updated
         // parameter 4, listen
-        // Remarks: For parameters 2 and 3, if parameter 3 is not 0, then parameter 3 shall prevail; if parameter 3 is 0, it will be updated regularly by time; if both are 0, it will be refreshed at any time
+        // Remarks: For parameters 2 and 3, if parameter 3 is not 0, then parameter 3 shall prevail;
+        // if parameter 3 is 0, it will be updated regularly by time; if both are 0,
+        // it will be refreshed at any time
 
         // Update once every 1 second, or once the minimum displacement change exceeds 1 meter;
         // Note: The update accuracy here is very low, it is recommended to start a Thread in the service, sleep(10000) in run; then execute handler.sendMessage() to update the location
